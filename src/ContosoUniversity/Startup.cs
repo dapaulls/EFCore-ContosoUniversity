2140ca1889c523d9cec2f132153c0b0958a06884
davidp <dapaulls@gmail.com>
2017-01-04 14:29:16 +0000

    Add code to initialize the database with test data

--- src/ContosoUniversity/Startup.cs
+++ src/ContosoUniversity/Startup.cs
@@ -58,7 +58,7 @@ namespace ContosoUniversity
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SchoolContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -86,6 +86,8 @@ namespace ContosoUniversity
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbInitializer.Initialize(context);
         }
     }
 }
